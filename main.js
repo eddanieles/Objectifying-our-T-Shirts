ad4177ed54274b3eb49562c97ff01a6a5e67ecbe
eddanieles <ed.danieles@yahoo.com>
2016-07-27 22:04:01 -0400

    updated files

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 var cartArray = [];
 
-//var shirtObject = {name: name, price: price, datetime: datetime};
+//var totalprice = 0;
 
-var addToCart = document.querySelector(".fa-shopping-cart");
+var addToCartBlue = document.querySelector("#blueCart");
 
-addToCart.addEventListener("click", function(event){
+addToCartBlue.addEventListener("click", function(event){
   var blueShirtObject = {name: "Save my trees", price: 29};
   cartArray.push(blueShirtObject);
-
-
 
   var item = document.createElement("li");
   item.innerText = blueShirtObject.name;
@@ -30,6 +28,7 @@ addToCart.addEventListener("click", function(event){
 
   for (i = 0; i < cartArray.length; i++){
     totalprice = totalprice + cartArray[i].price;
+    console.log(cartArray[i].price);
   }
 
   var total = document.querySelector("#totalprice");
@@ -37,46 +36,79 @@ addToCart.addEventListener("click", function(event){
 
 })
 
+var addToCartPink = document.querySelector("#pinkCart");
+
+addToCartPink.addEventListener("click", function(event){
+  var pinkShirtObject = {name: "Nature Lover", price: 19};
+  cartArray.push(pinkShirtObject);
+
+  var item = document.createElement("li");
+  item.innerText = pinkShirtObject.name;
+
+  var list = document.querySelector("#cart_list");
+  list.appendChild(item);
+
+  var item2 = document.createElement("li");
+  item2.innerText = pinkShirtObject.price;
+  list.appendChild(item2);
+
+  var now = new Date ();
+  pinkShirtObject.timeAdded = now;
+  var item3 = document.createElement("li");
+  item3.innerText = now;
+  list.appendChild(item3);
+
+  var totalprice = 0;
+
+  for (i = 0; i < cartArray.length; i++){
+      totalprice = totalprice + cartArray[i].price;
+      console.log(cartArray[i].price);
+  }
+
+  var total = document.querySelector("#totalprice");
+  total.innerText = totalprice;
+})
+
+var addToCartYellow = document.querySelector("#yellowCart");
+
+addToCartYellow.addEventListener("click", function(event){
+  var yellowShirtObject = {name: "Forest Walk", price: 39};
+  cartArray.push(yellowShirtObject);
+
+  var item = document.createElement("li");
+  item.innerText = yellowShirtObject.name;
+
+  var list = document.querySelector("#cart_list");
+  list.appendChild(item);
+
+  var item2 = document.createElement("li");
+  item2.innerText = yellowShirtObject.price;
+  list.appendChild(item2);
+
+  var now = new Date ();
+  yellowShirtObject.timeAdded = now;
+  var item3 = document.createElement("li");
+  item3.innerText = now;
+  list.appendChild(item3);
+
+  var totalprice = 0;
+
+  for (i = 0; i < cartArray.length; i++){
+      totalprice = totalprice + cartArray[i].price;
+      console.log(cartArray[i].price);
+  }
+
+  var total = document.querySelector("#totalprice");
+  total.innerText = totalprice;
+})
+
+
+
+
+
 
 
 var checkout = document.querySelector("#b_total");
 checkout.addEventListener("click", function(event){
     var confirm = window.confirm("Please Confirm Your Purchase");
 })
-
-
-
-/*
-<script>
-  var arr = [];
-  var form = document.querySelector("form");
-  form.addEventListener("submit", function(event){
-    event.preventDefault();
-    var name = form.querySelector("input[type='text']").value;
-    var urgency = form.querySelector("input[type='checkbox']").checked;
-    var list = document.querySelector("ul");
-    var item = document.createElement("li");
-    item.innerText = name;
-    if(urgency == true){
-      var note = document.createElement("strong");
-      note.innerText = "(URGENT)";
-      item.appendChild(note);
-    }
-    list.appendChild(item);
-
-
-
-    var myObject = {todo: name, isUrgent: urgency};
-    arr.push(myObject);
-
-  })
-
-  var save_todos = document.querySelector("#save");
-
-  save_todos.addEventListener("click", function(event){
-
-    console.log(arr);
-  })
-
-</script>
-*/
